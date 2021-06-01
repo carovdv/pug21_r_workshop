936f1e7f3d7a8c13eb4b7990d87fa113f23ef99a
carovdv <caroline.van-der-velde@uni-bamberg.de>
2021-06-01 12:00:39 +0200

    cleaning up

--- script_0.R
+++ script_0.R
@@ -1,2 +1,8 @@
+## File -> New Project -> Version Control 
+## Änderungen im Project "pushen" -> anhaken -> "commit" und eingeben, was geändert wurde 
+## your branch is ahead of origin/main by 1 commit: ändern, indem man auf push klickt
 
-"hello world"
+## was passiert, wenn ich etwas ändere? 
+## immer wieder auf "commit" und dann "push" klicken 
+
+## Vorteile: alten Versionen wiederherstellen und Code mit anderen Personen teilen
